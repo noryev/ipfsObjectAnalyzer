778cc212149aa239d84d118dc4cdc59b9948712d
Your Name <logan.lentz1@gmail.com>
2023-07-12 16:07:51 -0500

    adding error handling

--- src/index.js
+++ src/index.js
@@ -1,54 +1,64 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cid: '',
-      gateways: ['https://ipfs.io', 'https://leto.gg/ipfs/', 'https://cloudflare-ipfs.com'],
-      results: []
-    };
-  }
-
-  handleCIDChange = (event) => {
-    this.setState({ cid: event.target.value });
-  }
-
-  handleGatewayChange = (index, event) => {
-    const newGateways = this.state.gateways.map((gateway, idx) => idx === index ? event.target.value : gateway);
-    this.setState({ gateways: newGateways });
-  }
-
-  benchmark = async () => {
-    const results = [];
-    for (const gateway of this.state.gateways) {
-      const start = performance.now();
-      await fetch(`${gateway}/ipfs/${this.state.cid}`);
-      const end = performance.now();
-      const duration = end - start;
-      results.push({ gateway, duration });
-    }
-    this.setState({ results });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>IPFS CID Benchmarking</h1>
-        <input type="text" value={this.state.cid} onChange={this.handleCIDChange} placeholder="Content Identifier" />
-        {this.state.gateways.map((gateway, index) => (
-          <input key={index} type="text" value={gateway} onChange={(e) => this.handleGatewayChange(index, e)} />
-        ))}
-        <button onClick={this.benchmark}>Benchmark</button>
-        <ul>
-          {this.state.results.map((result, index) => (
-            <li key={index}>{result.gateway}: {result.duration}ms</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+      import ReactDOM from 'react-dom';
+      
+      class App extends Component {
+          constructor(props) {
+              super(props);
+              this.state = { 
+                  cid: '', 
+                  gateways: ['https://ipfs.io', 'https://cloudflare-ipfs.com', 'https://leto.gg/ipfs/'], 
+                  results: [], 
+                  loading: false, 
+                  error: null 
+              };
+          }
+      
+          handleCIDChange = (event) => {
+              this.setState({ cid: event.target.value });
+          }
+      
+          handleGatewayChange = (index, event) => {
+              const newGateways = this.state.gateways.map((gateway, idx) => idx === index ? event.target.value : gateway);
+              this.setState({ gateways: newGateways });
+          }
+      
+          benchmark = async () => {
+              this.setState({ loading: true, error: null });
+              const results = [];
+              try {
+                  for (const gateway of this.state.gateways) {
+                      const start = performance.now();
+                      await fetch(`${gateway}/ipfs/${this.state.cid}`);
+                      const end = performance.now();
+                      const duration = end - start;
+                      results.push({ gateway, duration });
+                  }
+              } catch(error) {
+                  this.setState({ error: error.message });
+              } finally {
+                  this.setState({ results, loading: false });
+              }
+          }
+      
+          render() {
+              return (
+                  <div>
+                      <h1>IPFS CID Benchmarking</h1>
+                      <input type="text" value={this.state.cid} onChange={this.handleCIDChange} placeholder="Content Identifier" />
+                      {this.state.gateways.map((gateway, index) => (
+                          <input key={index} type="text" value={gateway} onChange={(e) => this.handleGatewayChange(index, e)} />
+                      ))}
+                      <button onClick={this.benchmark}>Benchmark</button>
+                      {this.state.loading && <p>Loading...</p>}
+                      {this.state.error && <p>Error: {this.state.error}</p>}
+                      <ul>
+                          {this.state.results.map((result, index) => (
+                              <li key={index}>{result.gateway}: {result.duration}ms</li>
+                          ))}
+                      </ul>
+                  </div>
+              );
+          }
+      }
+      
+      ReactDOM.render(<App />, document.getElementById('root'));
