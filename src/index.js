796296e18b4cf279425ecbc3ef5c1d8d7111a6e0
noryev <Logan.lentz1@gmail.com>
2023-07-27 00:42:38 -0500

    backend file added

--- src/index.js
+++ src/index.js
@@ -64,4 +64,6 @@ class App extends Component {
 //Start adding more gateways to retrieve from (Fleek,Lighthouse.Storage)
 
 //Move infra files out of this React App for the UI!
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
