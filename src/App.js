4e1d79120e3157512ce551fde6de61685c31165c
Logan Lentz <logan.lentz1@gmail.com>
2023-10-03 16:34:27 -0700

    index.html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cid, setCid] = useState(''); // To store the CID from the input
+  const [message, setMessage] = useState(''); // To store messages or results from the worker
+
+  async function processCID() {
+    try {
+      const response = await fetch('https://worker-ipfs-analyze.deanlaughing.workers.dev/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cid }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Worker responded with status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setMessage(result.message);
+    } catch (error) {
+      setMessage(`Error: ${error.message}`);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>IPFS CID Processor</h1>
+        <input
+          type="text"
+          placeholder="Enter IPFS CID"
+          value={cid}
+          onChange={e => setCid(e.target.value)}
+        />
+        <button onClick={processCID}>Process CID</button>
+        <div className="progress" style={{ width: `${message.includes('100') ? 100 : 0}%` }}></div>
+        <p>{message}</p>
+      </div>
     </div>
   );
 }
